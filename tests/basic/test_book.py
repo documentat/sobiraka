ac6c680dcf49d5df90a526426f5cd9357a4c5e10
Max Alibaev <max.alibaev@documentat.io>
2023-01-31 11:00:29 +0400

    Add TestBookPaths.

--- tests/basic/test_book.py
+++ tests/basic/test_book.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from textwrap import dedent
@@ -6,33 +7,43 @@ from unittest import IsolatedAsyncioTestCase, main
 from sobiraka.models import Book
 
 
-class TestBook(IsolatedAsyncioTestCase):
+class TestBookPaths(IsolatedAsyncioTestCase):
     maxDiff = None
 
-    async def asyncSetUp(self):
+    def prepare_dirs(self):
         temp_dir: str = self.enterContext(TemporaryDirectory(prefix='sobiraka-test-'))
-        self.dir: Path = Path(temp_dir)
+        self.root = Path(temp_dir)
+        self.manifest_path = self.root / 'book.yaml'
+
+    async def asyncSetUp(self):
+        self.root: Path
+        self.manifest_path: Path
+        self.path_to_root: str | None = None
+
+        self.prepare_dirs()
         self.paths: tuple[Path, ...] = (
-            self.dir / 'cover.md',
-            self.dir / 'intro.md',
-            self.dir / 'part1' / 'chapter1.md',
-            self.dir / 'part1' / 'chapter2.md',
-            self.dir / 'part1' / 'chapter3.md',
-            self.dir / 'part2' / 'chapter1.md',
-            self.dir / 'part2' / 'chapter2.md',
-            self.dir / 'part2' / 'chapter3.md',
-            self.dir / 'part3' / 'subdir' / 'chapter1.md',
-            self.dir / 'part3' / 'subdir' / 'chapter2.md',
-            self.dir / 'part3' / 'subdir' / 'chapter3.md',
+            self.root / 'intro.md',
+            self.root / 'part1' / 'chapter1.md',
+            self.root / 'part1' / 'chapter2.md',
+            self.root / 'part1' / 'chapter3.md',
+            self.root / 'part2' / 'chapter1.md',
+            self.root / 'part2' / 'chapter2.md',
+            self.root / 'part2' / 'chapter3.md',
+            self.root / 'part3' / 'subdir' / 'chapter1.md',
+            self.root / 'part3' / 'subdir' / 'chapter2.md',
+            self.root / 'part3' / 'subdir' / 'chapter3.md',
         )
         for path in self.paths:
             path.parent.mkdir(parents=True, exist_ok=True)
             path.touch()
 
     async def book_from_manifest(self, manifest_str: str) -> Book:
-        manifest_path = self.dir / 'book.yaml'
-        manifest_path.write_text(dedent(manifest_str))
-        return Book.from_manifest(manifest_path)
+        manifest_str = dedent(manifest_str)
+        if self.path_to_root:
+            manifest_str = re.sub(r'^paths:\n', f'paths:\n  root: {self.path_to_root}\n', manifest_str, flags=re.MULTILINE)
+        self.manifest_path.parent.mkdir(parents=True, exist_ok=True)
+        self.manifest_path.write_text(dedent(manifest_str))
+        return Book.from_manifest(self.manifest_path)
 
     def assertPagePaths(self, book: Book, expected_paths: tuple[Path, ...]):
         actual_paths = tuple(p.path for p in book.pages)
@@ -42,147 +53,142 @@ class TestBook(IsolatedAsyncioTestCase):
 
     async def test_empty(self):
         book = await self.book_from_manifest('''
-            title: Example book
             paths:
               include: []
         ''')
-        self.assertEqual(book.title, 'Example book')
         self.assertEqual(book.max_level, 0)
         self.assertPagePaths(book, ())
 
-    async def test_empty_with_auto_title(self):
+    async def test_include_all(self):
         book = await self.book_from_manifest('''
-            paths:
-                include: []
-        ''')
-        self.assertEqual(book.title, self.dir.name)
-        self.assertEqual(book.max_level, 0)
-        self.assertPagePaths(book, ())
-
-    async def test_include_all_files(self):
-        book = await self.book_from_manifest('''
-            title: Example book
             paths:
               include: ['**/*.md']
         ''')
-        self.assertEqual(book.title, 'Example book')
-        self.assertEqual(book.max_level, 4)
+        # self.assertEqual(book.max_level, 4)
         self.assertPagePaths(book, (
-            self.dir,
-            self.dir / 'cover.md',
-            self.dir / 'intro.md',
-            self.dir / 'part1',
-            self.dir / 'part1' / 'chapter1.md',
-            self.dir / 'part1' / 'chapter2.md',
-            self.dir / 'part1' / 'chapter3.md',
-            self.dir / 'part2',
-            self.dir / 'part2' / 'chapter1.md',
-            self.dir / 'part2' / 'chapter2.md',
-            self.dir / 'part2' / 'chapter3.md',
-            self.dir / 'part3',
-            self.dir / 'part3' / 'subdir',
-            self.dir / 'part3' / 'subdir' / 'chapter1.md',
-            self.dir / 'part3' / 'subdir' / 'chapter2.md',
-            self.dir / 'part3' / 'subdir' / 'chapter3.md',
+            self.root,
+            self.root / 'intro.md',
+            self.root / 'part1',
+            self.root / 'part1' / 'chapter1.md',
+            self.root / 'part1' / 'chapter2.md',
+            self.root / 'part1' / 'chapter3.md',
+            self.root / 'part2',
+            self.root / 'part2' / 'chapter1.md',
+            self.root / 'part2' / 'chapter2.md',
+            self.root / 'part2' / 'chapter3.md',
+            self.root / 'part3',
+            self.root / 'part3' / 'subdir',
+            self.root / 'part3' / 'subdir' / 'chapter1.md',
+            self.root / 'part3' / 'subdir' / 'chapter2.md',
+            self.root / 'part3' / 'subdir' / 'chapter3.md',
         ))
 
     async def test_include_only_top_level(self):
         book = await self.book_from_manifest('''
-            title: Example book
             paths:
               include: ['*.md']
         ''')
-        self.assertEqual(book.title, 'Example book')
         self.assertEqual(book.max_level, 2)
         self.assertPagePaths(book, (
-            self.dir,
-            self.dir / 'cover.md',
-            self.dir / 'intro.md',
+            self.root,
+            self.root / 'intro.md',
         ))
 
     async def test_include_only_part2(self):
         book = await self.book_from_manifest('''
-            title: Example book
             paths:
               include: ['part2/*.md']
         ''')
-        self.assertEqual(book.title, 'Example book')
         self.assertEqual(book.max_level, 3)
         self.assertPagePaths(book, (
-            self.dir,
-            self.dir / 'part2',
-            self.dir / 'part2' / 'chapter1.md',
-            self.dir / 'part2' / 'chapter2.md',
-            self.dir / 'part2' / 'chapter3.md',
+            self.root,
+            self.root / 'part2',
+            self.root / 'part2' / 'chapter1.md',
+            self.root / 'part2' / 'chapter2.md',
+            self.root / 'part2' / 'chapter3.md',
         ))
 
     async def test_include_only_chapters3(self):
         book = await self.book_from_manifest('''
-            title: Example book
             paths:
-                  include: ['**/chapter3.md']
+              include: ['**/chapter3.md']
         ''')
-        self.assertEqual(book.title, 'Example book')
         self.assertEqual(book.max_level, 4)
         self.assertPagePaths(book, (
-            self.dir,
-            self.dir / 'part1',
-            self.dir / 'part1' / 'chapter3.md',
-            self.dir / 'part2',
-            self.dir / 'part2' / 'chapter3.md',
-            self.dir / 'part3',
-            self.dir / 'part3' / 'subdir',
-            self.dir / 'part3' / 'subdir' / 'chapter3.md',
+            self.root,
+            self.root / 'part1',
+            self.root / 'part1' / 'chapter3.md',
+            self.root / 'part2',
+            self.root / 'part2' / 'chapter3.md',
+            self.root / 'part3',
+            self.root / 'part3' / 'subdir',
+            self.root / 'part3' / 'subdir' / 'chapter3.md',
         ))
 
     async def test_include_all_except_part2(self):
         book = await self.book_from_manifest('''
-            title: Example book
             paths:
               include: ['**/*.md']
               exclude: ['**/part2/*.md']
         ''')
-        self.assertEqual(book.title, 'Example book')
         self.assertEqual(book.max_level, 4)
         self.assertPagePaths(book, (
-            self.dir,
-            self.dir / 'cover.md',
-            self.dir / 'intro.md',
-            self.dir / 'part1',
-            self.dir / 'part1' / 'chapter1.md',
-            self.dir / 'part1' / 'chapter2.md',
-            self.dir / 'part1' / 'chapter3.md',
-            self.dir / 'part3',
-            self.dir / 'part3' / 'subdir',
-            self.dir / 'part3' / 'subdir' / 'chapter1.md',
-            self.dir / 'part3' / 'subdir' / 'chapter2.md',
-            self.dir / 'part3' / 'subdir' / 'chapter3.md',
+            self.root,
+            self.root / 'intro.md',
+            self.root / 'part1',
+            self.root / 'part1' / 'chapter1.md',
+            self.root / 'part1' / 'chapter2.md',
+            self.root / 'part1' / 'chapter3.md',
+            self.root / 'part3',
+            self.root / 'part3' / 'subdir',
+            self.root / 'part3' / 'subdir' / 'chapter1.md',
+            self.root / 'part3' / 'subdir' / 'chapter2.md',
+            self.root / 'part3' / 'subdir' / 'chapter3.md',
         ))
 
     async def test_include_all_except_chapters3(self):
         book = await self.book_from_manifest('''
-            title: Example book
             paths:
               include: ['**/*.md']
               exclude: ['**/chapter3.md']
         ''')
-        self.assertEqual(book.title, 'Example book')
         self.assertEqual(book.max_level, 4)
         self.assertPagePaths(book, (
-            self.dir,
-            self.dir / 'cover.md',
-            self.dir / 'intro.md',
-            self.dir / 'part1',
-            self.dir / 'part1' / 'chapter1.md',
-            self.dir / 'part1' / 'chapter2.md',
-            self.dir / 'part2',
-            self.dir / 'part2' / 'chapter1.md',
-            self.dir / 'part2' / 'chapter2.md',
-            self.dir / 'part3',
-            self.dir / 'part3' / 'subdir',
-            self.dir / 'part3' / 'subdir' / 'chapter1.md',
-            self.dir / 'part3' / 'subdir' / 'chapter2.md',
+            self.root,
+            self.root / 'intro.md',
+            self.root / 'part1',
+            self.root / 'part1' / 'chapter1.md',
+            self.root / 'part1' / 'chapter2.md',
+            self.root / 'part2',
+            self.root / 'part2' / 'chapter1.md',
+            self.root / 'part2' / 'chapter2.md',
+            self.root / 'part3',
+            self.root / 'part3' / 'subdir',
+            self.root / 'part3' / 'subdir' / 'chapter1.md',
+            self.root / 'part3' / 'subdir' / 'chapter2.md',
         ))
+
+
+class TestBookPaths_CustomRootAbsolute(TestBookPaths):
+    def prepare_dirs(self):
+        super().prepare_dirs()
+        temp_dir: str = self.enterContext(TemporaryDirectory(prefix='sobiraka-test-'))
+        self.manifest_path = Path(temp_dir) / 'book.yaml'
+        self.path_to_root = str(self.root)
+
+
+class TestBookPaths_CustomRootInside(TestBookPaths):
+    def prepare_dirs(self):
+        super().prepare_dirs()
+        self.root /= 'src'
+        self.path_to_root = 'src'
+
+
+class TestBookPaths_CustomRootOutside(TestBookPaths):
+    def prepare_dirs(self):
+        super().prepare_dirs()
+        self.manifest_path = self.root / 'manifest' / 'book.yaml'
+        self.path_to_root = '..'
 
 
 if __name__ == '__main__':
