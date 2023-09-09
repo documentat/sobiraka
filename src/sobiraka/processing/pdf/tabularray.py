37b90b71967ff02833572a845c693592aaf7dd52
Max Alibaev <max.alibaev@documentat.io>
2023-09-09 18:04:51 +0400

    In TabulArrayProcessor, make separate function _make_cell().

--- src/sobiraka/processing/pdf/tabularray.py
+++ src/sobiraka/processing/pdf/tabularray.py
@@ -14,79 +14,34 @@ from ..replacement import TableReplPara
 class TabulArrayProcessor(Dispatcher):
 
     async def process_table(self, table: Table, page: Page) -> tuple[Element, ...]:
-        # pylint: disable=too-many-branches
-        # pylint: disable=too-many-locals
-        # pylint: disable=too-many-statements
-
         table = await self.process_container(table, page)
         assert isinstance(table, Table)
 
-        para = TableReplPara(table)
-        result = [para]
+        result = [TableReplPara(table)]
 
-        table_square_bracket_options = list(self.tabularray_table_square_bracket_options(table))
-        table_curly_bracket_options = list(self.tabularray_table_curly_bracket_options(table))
-        para.content.append(LatexInline(r'\begin{tblr}'
-                                        + '[' + ','.join(table_square_bracket_options) + ']'
-                                        + '{' + ','.join(table_curly_bracket_options) + '}'))
-        para.content.append(Str('\n'))
+        result[-1].content.append(LatexInline(r'\begin{tblr}'
+                                        + '[' + ','.join(self.tabularray_table_square_bracket_options(table)) + ']'
+                                        + '{' + ','.join(self.tabularray_table_curly_bracket_options(table)) + '}'))
+        result[-1].content.append(Str('\n'))
 
         assert len(table.head.content) == 1, 'This class does not support multi-row table heads yet.'
         head_row = table.head.content[0]
         for i, head_cell in enumerate(head_row.content):
             assert head_cell.colspan == 1, 'This class does not support multi-column cells yet.'
-            para.content.append(Strong(*head_cell.content[0].content))
-            para.content.append(Space())
+            result[-1].content.append(Strong(*head_cell.content[0].content))
+            result[-1].content.append(Space())
             if i < len(head_row.content) - 1:
-                para.content += LatexInline('&'), Str('\n')
-        para.content += Str('\n'), LatexInline('\\\\'), Str('\n')
+                result[-1].content += LatexInline('&'), Str('\n')
+        result[-1].content += Str('\n'), LatexInline('\\\\'), Str('\n')
 
         for i, row in enumerate(_grid(table)):
             do_not_break_table = False
             for j, grid_item in enumerate(row):
                 match grid_item:
                     case CellPlacement() as cell_placement:
-                        cell = cell_placement.cell
-                        assert cell.colspan == 1, 'This class does not support multi-column cells yet.'
-
-                        cell_square_bracket_options = list(self.tabularray_cell_square_bracket_options(cell_placement))
-                        cell_curly_bracket_options = list(self.tabularray_cell_curly_bracket_options(cell_placement))
-                        if cell.rowspan > 1:
-                            cell_square_bracket_options.append(f'r={cell.rowspan}')
+                        self._make_cell(cell_placement, table, result)
+                        if cell_placement.cell.rowspan > 1:
                             do_not_break_table = True
-                        if cell_square_bracket_options or cell_curly_bracket_options:
-                            set_cell = r'\SetCell'
-                            if cell_square_bracket_options:
-                                set_cell += '[' + ','.join(cell_square_bracket_options) + ']'
-                            set_cell += '{' + ','.join(cell_curly_bracket_options) + '}'
-                            para.content += Space(), LatexInline(set_cell)
-
-                        para.content += LatexInline('{'), Space()
-
-                        if len(cell.content) == 1 and isinstance(cell.content[0], Plain):
-                            for inline_item in cell.content[0].content:
-                                if isinstance(inline_item, LineBreak):
-                                    para.content += Space(), LatexInline('\\\\'), Space()
-                                else:
-                                    para.content.append(inline_item)
-
-                        else:
-                            para.content += Str('\n'), LatexInline('% BEGIN STRIP')
-                            for block_item in cell.content:
-                                if isinstance(block_item, BulletList):
-                                    result += \
-                                        LatexBlock(r'\begin{tcolorbox}[size=tight,opacityframe=0,opacityback=0]'), \
-                                            block_item, \
-                                            LatexBlock(r'\end{tcolorbox}'), \
-                                            TableReplPara(table)
-                                    para = result[-1]
-                                else:
-                                    result.append(block_item)
-                                    result.append(TableReplPara(table))
-                                    para = result[-1]
-                            para.content += LatexInline('% END STRIP'), Str('\n')
-
-                        para.content += Space(), LatexInline('}')
 
                     case CellContinuation() as cell_continuation:
                         if not cell_continuation.is_last_row:
@@ -94,17 +49,57 @@ class TabulArrayProcessor(Dispatcher):
 
                 if j == len(row) - 1:
                     if do_not_break_table:
-                        para.content += Str('\n'), LatexInline('\\\\*'), Str('\n')
+                        result[-1].content += Str('\n'), LatexInline('\\\\*'), Str('\n')
                     else:
-                        para.content += Str('\n'), LatexInline('\\\\'), Str('\n')
+                        result[-1].content += Str('\n'), LatexInline('\\\\'), Str('\n')
                 else:
-                    para.content += Space(), LatexInline('&'), Str('\n')
+                    result[-1].content += Space(), LatexInline('&'), Str('\n')
 
-        para.content.append(LatexInline(r'''
+        result[-1].content.append(LatexInline(r'''
              \end{tblr}
         '''))
 
         return tuple(result)
+
+    def _make_cell(self, cell_placement: CellPlacement, table: Table, result: list[TableReplPara]):
+        cell = cell_placement.cell
+        assert cell.colspan == 1, 'This class does not support multi-column cells yet.'
+
+        cell_square_bracket_options = list(self.tabularray_cell_square_bracket_options(cell_placement))
+        cell_curly_bracket_options = list(self.tabularray_cell_curly_bracket_options(cell_placement))
+        if cell.rowspan > 1:
+            cell_square_bracket_options.append(f'r={cell.rowspan}')
+        if cell_square_bracket_options or cell_curly_bracket_options:
+            set_cell = r'\SetCell'
+            if cell_square_bracket_options:
+                set_cell += '[' + ','.join(cell_square_bracket_options) + ']'
+            set_cell += '{' + ','.join(cell_curly_bracket_options) + '}'
+            result[-1].content += Space(), LatexInline(set_cell)
+
+        result[-1].content += LatexInline('{'), Space()
+
+        if len(cell.content) == 1 and isinstance(cell.content[0], Plain):
+            for inline_item in cell.content[0].content:
+                if isinstance(inline_item, LineBreak):
+                    result[-1].content += Space(), LatexInline('\\\\'), Space()
+                else:
+                    result[-1].content.append(inline_item)
+
+        else:
+            result[-1].content += Str('\n'), LatexInline('% BEGIN STRIP')
+            for block_item in cell.content:
+                if isinstance(block_item, BulletList):
+                    result += \
+                        LatexBlock(r'\begin{tcolorbox}[size=tight,opacityframe=0,opacityback=0]'), \
+                            block_item, \
+                            LatexBlock(r'\end{tcolorbox}'), \
+                            TableReplPara(table)
+                else:
+                    result.append(block_item)
+                    result.append(TableReplPara(table))
+            result[-1].content += LatexInline('% END STRIP'), Str('\n')
+
+        result[-1].content += Space(), LatexInline('}')
 
     def tabularray_colspec(self, table: Table) -> Sequence[str]:
         return 'X' * table.cols
