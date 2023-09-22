33b9249ff631958896e8f585730ca49ebaa1b41f
Max Alibaev <max.alibaev@documentat.io>
2023-09-22 14:13:41 +0400

    New helper classes: HtmlBlock, HtmlInline.

--- src/sobiraka/utils/__init__.py
+++ src/sobiraka/utils/__init__.py
@@ -1,9 +1,9 @@
 from .convert_or_none import convert_or_none
 from .debug import save_debug_json
-from .latex import LatexBlock, LatexInline
 from .merge_dicts import merge_dicts
 from .on_demand import on_demand
 from .panflute_utils import panflute_to_bytes
+from .raw import HtmlBlock, HtmlInline, LatexBlock, LatexInline
 from .render import render
 from .unique_list import UniqueList
 from .validate_dictionary import validate_dictionary
--- src/sobiraka/utils/raw.py
+++ src/sobiraka/utils/raw.py
@@ -3,6 +3,21 @@ from textwrap import dedent
 from panflute import RawBlock, RawInline
 
 
+class HtmlBlock(RawBlock):
+    tag = 'RawBlock'
+
+    def __init__(self, text: str):
+        super().__init__(dedent(text).strip(), 'html')
+
+
+class HtmlInline(RawInline):
+    tag = 'RawInline'
+
+    def __init__(self, text: str):
+        super().__init__(dedent(text).strip(), 'html')
+
+
+
 class LatexBlock(RawBlock):
     tag = 'RawBlock'
 
