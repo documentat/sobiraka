85455016b1c7746a58256f7ddd7a323a05301907
Max Alibaev <max.alibaev@documentat.io>
2024-11-01 14:00:33 +0400

    Rename in FakeFileSystem: _files — pseudofiles.

--- tests/helpers/fakefilesystem.py
+++ tests/helpers/fakefilesystem.py
@@ -9,41 +9,41 @@ from sobiraka.utils import AbsolutePath, RelativePath
 
 
 class FakeFileSystem(FileSystem):
-    def __init__(self):
-        self._files: dict[RelativePath, bytes | str] = {}
+    def __init__(self, pseudofiles: dict[RelativePath, bytes | str] = None):
+        self.pseudofiles: dict[RelativePath, bytes | str] = pseudofiles or {}
 
     def __setitem__(self, key: RelativePath | str, value: bytes | str):
-        self._files = dict(sorted((*self._files.items(), (RelativePath(key), value))))
+        self.pseudofiles = dict(sorted((*self.pseudofiles.items(), (RelativePath(key), value))))
 
     def exists(self, path: RelativePath) -> bool:
-        return path in self._files
+        return path in self.pseudofiles
 
     def is_dir(self, path: RelativePath) -> bool:
-        if path in self._files:
+        if path in self.pseudofiles:
             return False
-        for result in self._files.keys():
+        for result in self.pseudofiles.keys():
             if result.parts[:len(path.parts)] == path.parts:
                 return True
         return False
 
     @contextmanager
     def open_bytes(self, path: RelativePath) -> AbstractContextManager[BinaryIO]:
-        yield BytesIO(self._files[path])
+        yield BytesIO(self.pseudofiles[path])
 
     @contextmanager
     def open_text(self, path: RelativePath) -> AbstractContextManager[TextIO]:
-        yield StringIO(self._files[path])
+        yield StringIO(self.pseudofiles[path])
 
     def copy(self, source: RelativePath, target: AbsolutePath):
         target.parent.mkdir(parents=True, exist_ok=True)
-        match self._files[source]:
+        match self.pseudofiles[source]:
             case bytes() as data:
                 target.write_bytes(data)
             case str() as data:
                 target.write_text(data)
 
     def glob(self, path: RelativePath, pattern: str) -> Iterable[RelativePath]:
-        for result in self._files.keys():
+        for result in self.pseudofiles.keys():
             part1 = RelativePath(*result.parts[:len(path.parts)])
             part2 = RelativePath(*result.parts[len(path.parts):])
             if part1 == path and globmatch(part2, pattern, **GLOB_KWARGS):
