9174b87780d6db702bc34607f5f7682f0d14b221
Max Alibaev <max.alibaev@documentat.io>
2025-05-14 15:18:04 +0400

    Minor refactoring and commenting in `SourceNav`.

--- src/sobiraka/models/source/sourcenav.py
+++ src/sobiraka/models/source/sourcenav.py
@@ -30,32 +30,36 @@ class SourceNav(Source):
         naming_scheme = self.volume.config.paths.naming_scheme
 
         child_sources = []
+        for subpath, options in self._data['items']:
 
-        items = self._data.get('items', ())
-        items = list(map(self._parse_item, items))
-        for subpath, options in items:
-
+            # Make sure the specified file exists
             if not fs.exists(self.path_in_project / subpath):
                 self.issues.append(NonExistentFileInNav(subpath))
                 continue
 
+            # Make sure the specified file is not the index file
             if naming_scheme.parse(subpath).is_main:
                 self.issues.append(IndexFileInNav(subpath))
                 continue
 
+            # Generate the child
             child = make_source(self.volume, self.path_in_project / subpath, parent=self)
+            child_sources.append(child)
+
+            # Provide base options for the child, if specified in the item
             if options:
                 assert isinstance(child, SourceFile), 'Custom metadata is only applicable to a SourceFile'
                 child.base_meta = options
 
-            child_sources.append(child)
-
+        # Now look for the actual files in the directory:
+        # if there is an index file there, use it
         for index_path in fs.iterdir(self.path_in_project):
             if not fs.is_dir(index_path):
                 if naming_scheme.parse(index_path).is_main:
                     child_sources.insert(0, IndexSourceFile(self.volume, index_path, parent=self))
                     break
 
+        # Done
         self.child_sources = tuple(child_sources)
 
     @override
@@ -98,6 +102,9 @@ class SourceNav(Source):
         with fs.open_text(nav_path) as index_file:
             data = yaml.safe_load(index_file) or {}
             Draft202012Validator(data).validate(SCHEMA)
+
+        data.setdefault('items', [])
+        data['items'] = list(map(self._parse_item, data['items']))
 
         return data
 
