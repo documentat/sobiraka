71cc85a71a50b6d6a431263ba4465b35ee493665
Max Alibaev <max.alibaev@documentat.io>
2025-05-31 22:11:33 +0400

    Move `replace_element()` to `panflute_utils`.

--- src/sobiraka/utils/panflute_utils.py
+++ src/sobiraka/utils/panflute_utils.py
@@ -1,9 +1,18 @@
 from io import StringIO
 
 import panflute
+from panflute import Element
 
 
 def panflute_to_bytes(doc: panflute.Doc) -> bytes:
     with StringIO() as stringio:
         panflute.dump(doc, stringio)
         return stringio.getvalue().encode('utf-8')
+
+
+def replace_element(old: Element, new: Element | None):
+    pos = old.container.list.index(old)
+    if new is not None:
+        old.container.list[pos] = new
+    else:
+        del old.container.list[pos]
--- src/sobiraka/utils/replace_element.py
+++
@@ -1,9 +0,0 @@
-from panflute import Element
-
-
-def replace_element(old: Element, new: Element | None):
-    pos = old.container.list.index(old)
-    if new is not None:
-        old.container.list[pos] = new
-    else:
-        del old.container.list[pos]
